34cc2eecbee8a135dee96c7c4b72c3a781a8031c
LauD <marmuravlad10@gmail.com>
2026-04-01 20:38:57 +0300

    Placed @Data instead of getters and setters.

--- src/main/java/com/epam/finaltask/dto/UserDTO.java
+++ src/main/java/com/epam/finaltask/dto/UserDTO.java
@@ -6,6 +6,7 @@ import com.epam.finaltask.model.Voucher;
 
 import lombok.Data;
 
+@Data
 public class UserDTO {
 
 	private String id;
@@ -23,69 +24,5 @@ public class UserDTO {
 	private Double balance;
 
 	private boolean active;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public List<Voucher> getVouchers() {
-		return vouchers;
-	}
-
-	public void setVouchers(List<Voucher> vouchers) {
-		this.vouchers = vouchers;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public Double getBalance() {
-		return balance;
-	}
-
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
 	
 }
