96ddaedb629556c1e317e709edd3942da66b37e7
LauD <marmuravlad10@gmail.com>
2026-04-02 03:06:03 +0300

    Implemented all methods in UserServiceImpl.java.

--- src/main/java/com/epam/finaltask/service/UserServiceImpl.java
+++ src/main/java/com/epam/finaltask/service/UserServiceImpl.java
@@ -1,34 +1,66 @@
 package com.epam.finaltask.service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import com.epam.finaltask.dto.UserDTO;
+import com.epam.finaltask.exception.DuplicateRequestException;
+import com.epam.finaltask.mapper.UserMapper;
+import com.epam.finaltask.model.Role;
+import com.epam.finaltask.model.User;
+import com.epam.finaltask.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+	private final UserMapper userMapper;
+	private final UserRepository userRepository;
 
 	@Override
 	public UserDTO register(UserDTO userDTO) {
-		return null;
+		if(userRepository.existsByUsername(userDTO.getUsername())) {
+			throw new DuplicateRequestException("Username is already in use");
+		}
+		userRepository.save(userMapper.toUser(userDTO));
+		return userDTO;
 	}
 
 	@Override
 	public UserDTO updateUser(String username, UserDTO userDTO) {
-		return null;
+		User user = userRepository.findUserByUsername(username).orElseThrow(() -> new EntityNotFoundException("No such username"));
+		user.setUsername(userDTO.getUsername());
+		user.setPassword(userDTO.getPassword());
+		user.setRole(Role.valueOf(userDTO.getRole()));
+		user.setVouchers(userDTO.getVouchers());
+		user.setPhoneNumber(userDTO.getPhoneNumber());
+		user.setBalance(BigDecimal.valueOf(userDTO.getBalance()));
+		user.setAccountStatus(userDTO.isActive());
+		return userMapper.toUserDTO(user);
 	}
 
 	@Override
 	public UserDTO getUserByUsername(String username) {
-		return null;
+		User user = userRepository.findUserByUsername(username)
+				.orElseThrow(() -> new EntityNotFoundException("No such username"));
+		return userMapper.toUserDTO(user);
 	}
 
 	@Override
 	public UserDTO changeAccountStatus(UserDTO userDTO) {
-		return null;
+		User user = userRepository.findUserByUsername(userDTO.getUsername())
+				.orElseThrow(() -> new EntityNotFoundException("No such username"));
+		user.setAccountStatus(userDTO.isActive());
+		return userMapper.toUserDTO(user);
 	}
 
 	@Override
 	public UserDTO getUserById(UUID id) {
-		return null;
+		return userMapper.toUserDTO(userRepository.findUserById(id)
+				.orElseThrow(() -> new EntityNotFoundException("No such ID")));
 	}
 
 }
