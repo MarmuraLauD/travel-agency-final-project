c3eb552dbaec8b39f342730b2750d982af6d8524
LauD <marmuravlad10@gmail.com>
2026-04-03 06:59:35 +0300

    Implemented a basic mapping of user service's methods.

--- src/main/java/com/epam/finaltask/restcontroller/UserRestController.java
+++ src/main/java/com/epam/finaltask/restcontroller/UserRestController.java
@@ -1,10 +1,51 @@
 package com.epam.finaltask.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.epam.finaltask.dto.UserDTO;
+import com.epam.finaltask.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserRestController {
-	
+
+    private final UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.register(userDTO));
+    }
+
+    @PatchMapping("/{username}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("username") String username,
+                                             @RequestBody UserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.updateUser(username, userDTO));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.getUserByUsername(username));
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<UserDTO> changeAccountStatus(@PathVariable("id") String id, @RequestBody UserDTO userDTO) {
+        userDTO.setId(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.changeAccountStatus(userDTO));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.getUserById(UUID.fromString(id)));
+    }
+
 }
