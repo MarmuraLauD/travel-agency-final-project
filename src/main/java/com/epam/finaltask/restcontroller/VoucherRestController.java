ec4d3c4ec19650309c024aa89b5fe9eb11cf015d
LauD <marmuravlad10@gmail.com>
2026-04-03 19:50:05 +0300

    Refactored code to pass tests.

--- src/main/java/com/epam/finaltask/restcontroller/VoucherRestController.java
+++ src/main/java/com/epam/finaltask/restcontroller/VoucherRestController.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/vouchers")
@@ -19,63 +21,82 @@ public class VoucherRestController {
 
     private final VoucherService voucherService;
 
-    @PostMapping("/")
-    public ResponseEntity<VoucherDTO>  createVoucher(@RequestBody VoucherDTO voucherDTO){
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(voucherService.create(voucherDTO));
+    @PostMapping
+    public ResponseEntity<Map<String, String>> createVoucher(@RequestBody VoucherDTO voucherDTO){
+        voucherService.create(voucherDTO);
+        Map<String, String> response = new HashMap<>();
+        response.put("statusCode", "OK");
+        response.put("statusMessage", "Voucher is successfully created");
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<VoucherDTO> updateVoucher(@PathVariable("id") String id,
-                                                    @RequestBody VoucherDTO voucherDTO){
+    @PatchMapping("/{id}")
+    public ResponseEntity<Map<String, String>> updateVoucher(@PathVariable("id") String id,
+                                                             @RequestBody VoucherDTO voucherDTO){
         voucherDTO.setId(id);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(voucherService.update(id, voucherDTO));
+        voucherService.update(id, voucherDTO);
+        Map<String, String> response = new HashMap<>();
+        response.put("statusCode", "OK");
+        response.put("statusMessage", "Voucher is successfully updated");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteVoucher(@PathVariable("id") String id){
+    public ResponseEntity<Map<String, String>> deleteVoucher(@PathVariable("id") String id){
         voucherService.delete(id);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Map<String, String> response = new HashMap<>();
+        response.put("statusCode", "OK");
+        response.put("statusMessage", "Voucher with Id " + id + " has been deleted");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @PatchMapping("/{id}/hotStatus")
-    public ResponseEntity<VoucherDTO> changeHotStatus(@PathVariable("id") String id,
-                                                      @RequestBody VoucherDTO voucherDTO){
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Map<String, String>> changeHotStatus(@PathVariable("id") String id,
+                                                               @RequestBody VoucherDTO voucherDTO){
         voucherDTO.setId(id);
-        return ResponseEntity.status(HttpStatus.OK).body(voucherService.changeHotStatus(id, voucherDTO));
+        voucherService.changeHotStatus(id, voucherDTO);
+        Map<String, String> response = new HashMap<>();
+        response.put("statusCode", "OK");
+        response.put("statusMessage", "Voucher status is successfully changed");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PostMapping("/{id}/order")
     public ResponseEntity<VoucherDTO> order(@PathVariable("id") String id, @RequestParam String userId){
-        return  ResponseEntity.status(HttpStatus.OK)
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(voucherService.order(id, userId));
     }
 
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Map<String, Object>> getVouchersByUserId(@PathVariable("userId") String userId) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("results", voucherService.findAllByUserId(userId));
+        return ResponseEntity.ok(response);
+    }
+
     @GetMapping
-    public ResponseEntity<List<VoucherDTO>> getVouchersByParameter(@RequestParam(required = false) String userId,
-                                                       @RequestParam(required = false) String tourType,
-                                                       @RequestParam(required = false) String transferType,
-                                                       @RequestParam(required = false) String price,
-                                                       @RequestParam(required = false) String hotelType){
-        if(userId != null){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAllByUserId(userId));
-        }else if(tourType != null){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAllByTourType(TourType.valueOf(tourType)));
-        }else if(transferType != null){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAllByTransferType(transferType));
-        }else if(price != null){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAllByPrice(Double.parseDouble(price)));
-        }else if(hotelType != null){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAllByHotelType(HotelType.valueOf(hotelType)));
+    public ResponseEntity<Map<String, Object>> getVouchersByParameter(
+            @RequestParam(required = false) String tourType,
+            @RequestParam(required = false) String transferType,
+            @RequestParam(required = false) String price,
+            @RequestParam(required = false) String hotelType) {
+
+        List<VoucherDTO> results;
+
+        if (tourType != null) {
+            results = voucherService.findAllByTourType(TourType.valueOf(tourType));
+        } else if (transferType != null) {
+            results = voucherService.findAllByTransferType(transferType);
+        } else if (price != null) {
+            results = voucherService.findAllByPrice(Double.parseDouble(price));
+        } else if (hotelType != null) {
+            results = voucherService.findAllByHotelType(HotelType.valueOf(hotelType));
         } else {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(voucherService.findAll());
+            results = voucherService.findAll();
         }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("results", results);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
