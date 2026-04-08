635981873da01162c2db6affa7990723075bd338
LauD <marmuravlad10@gmail.com>
2026-04-08 14:01:01 +0300

    Added a refresh token entity.

---
+++ src/main/java/com/epam/finaltask/model/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.epam.finaltask.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String token;
+
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinTable(name = "user_id")
+    private User user;
+}
