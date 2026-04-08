8bce4cc4baafdb086b1a784dc1d624267faca7fc
LauD <marmuravlad10@gmail.com>
2026-04-08 14:49:11 +0300

    Implemented a userDetailService bean.

--- src/main/java/com/epam/finaltask/config/ApplicationConfig.java
+++ src/main/java/com/epam/finaltask/config/ApplicationConfig.java
@@ -1,24 +1,20 @@
 package com.epam.finaltask.config;
 
+import com.epam.finaltask.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserRepository userRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -26,17 +22,10 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/api/users").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            return userRepository.findUserByUsername(username)
+                    .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        };
     }
 }
