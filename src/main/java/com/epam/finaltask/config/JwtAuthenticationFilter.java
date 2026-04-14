3f675e2155b26c17cf50c757094c170c8b33572b
LauD <marmuravlad10@gmail.com>
2026-04-14 17:21:23 +0300

    Added a refresh token logic in filter chain.

--- src/main/java/com/epam/finaltask/config/JwtAuthenticationFilter.java
+++ src/main/java/com/epam/finaltask/config/JwtAuthenticationFilter.java
@@ -1,8 +1,12 @@
 package com.epam.finaltask.config;
 
 import com.epam.finaltask.mapper.UserMapper;
+import com.epam.finaltask.model.RefreshToken;
+import com.epam.finaltask.repository.RefreshTokenRepository;
 import com.epam.finaltask.service.UserService;
 import com.epam.finaltask.service.security.JwtService;
+import com.epam.finaltask.service.security.RefreshTokenService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -10,6 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,27 +31,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserService userService;
     private final UserMapper userMapper;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final RefreshTokenService refreshTokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull  FilterChain filterChain)
             throws ServletException, IOException {
         String jwt = null;
-        String authHeader = request.getHeader("Authorization");
-
-        if(authHeader != null && authHeader.startsWith("Bearer ")) {
-            jwt = authHeader.substring(7);
-        } else if (request.getCookies() != null) {
+        String refreshJwt = null;
+        if (request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
-                if ("jwt".equals(cookie.getName())) {
-                    jwt = cookie.getValue();
-                    break;
-                }
+                if ("jwt".equals(cookie.getName())) jwt = cookie.getValue();
+                if ("refresh_jwt".equals(cookie.getName())) refreshJwt = cookie.getValue();
             }
         }
-        if (jwt == null) {
+
+        if (jwt == null && refreshJwt == null) {
             filterChain.doFilter(request, response);
             return;
         }
+
+        if (jwt == null) {
+            RefreshToken refreshToken = refreshTokenRepository.findByToken(refreshJwt)
+                    .orElseThrow(() -> new EntityNotFoundException("Refresh token not found"));
+            if (!refreshTokenService.isExpired(refreshToken)) {
+                UserDetails userDetails = refreshToken.getUser();
+                jwt = jwtService.generateToken(userDetails);
+                Cookie newJwtCookie = new Cookie("jwt", jwt);
+                newJwtCookie.setPath("/");
+                newJwtCookie.setHttpOnly(true);
+                newJwtCookie.setMaxAge(jwtService.getMaxAgeSeconds());
+                response.addCookie(newJwtCookie);
+            }
+        }
+
         UserDetails userDetails = userMapper.toUser(userService.getUserByUsername(jwtService.extractUsername(jwt)));
         if (userDetails != null && jwtService.isTokenValid(jwt, userDetails)) {
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
@@ -59,5 +77,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
-
 }
