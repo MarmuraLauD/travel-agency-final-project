6b4ac3371a5ab4f3ed7bec2af3ea5cc9c6294184
LauD <marmuravlad10@gmail.com>
2026-04-14 17:30:04 +0300

    Added new methods.

--- src/main/java/com/epam/finaltask/service/VoucherService.java
+++ src/main/java/com/epam/finaltask/service/VoucherService.java
@@ -1,5 +1,6 @@
 package com.epam.finaltask.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,16 +13,19 @@ import org.springframework.data.domain.Pageable;
 
 public interface VoucherService {
     VoucherDTO create(VoucherDTO voucherDTO);
-    VoucherDTO order(String id, String userId);
+    VoucherDTO order(String id, String userId, LocalDate arrivalDate);
     VoucherDTO update(String id, VoucherDTO voucherDTO);
     void delete(String voucherId);
-    VoucherDTO changeHotStatus(String id, VoucherDTO voucherDTO);
+    void changeHotStatus(String id, boolean hot);
     List<VoucherDTO> findAllByUserId(UUID userId);
+    void cancelOrder(UUID voucherId);
 
     Page<VoucherDTO> findAllByTourType(TourType tourType, Pageable pageable);
     Page<VoucherDTO> findAllByTransferType(TransferType transferType, Pageable pageable);
     Page<VoucherDTO> findAllByPrice(Double price, Pageable pageable);
     Page<VoucherDTO> findAllByHotelType(HotelType hotelType, Pageable pageable);
+    Page<VoucherDTO> findAllByStatus(String status, Pageable pageable);
+
 
     Page<VoucherDTO> findAll(Pageable pageable);
 }
