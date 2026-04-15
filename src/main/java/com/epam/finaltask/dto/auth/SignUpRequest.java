a131ec794d9d5ac529af7993a92e356da8f5aede
LauD <marmuravlad10@gmail.com>
2026-04-15 15:09:31 +0300

    Added validate messages.

--- src/main/java/com/epam/finaltask/dto/auth/SignUpRequest.java
+++ src/main/java/com/epam/finaltask/dto/auth/SignUpRequest.java
@@ -1,21 +1,22 @@
 package com.epam.finaltask.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SignUpRequest {
 
-    @NotBlank
+    @NotBlank(message = "Username cannot be empty")
     @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "Password cannot be empty")
     @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
-    @NotNull
+    @NotBlank(message = "Phone number cannot be empty")
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone number format (10-15 digits)")
     private String phoneNumber;
 }
