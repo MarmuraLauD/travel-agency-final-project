74ea85931dc042705c25596eb78797271d1b50b7
LauD <marmuravlad10@gmail.com>
2026-04-15 15:12:24 +0300

    Added validate messages.

--- src/main/java/com/epam/finaltask/dto/VoucherDTO.java
+++ src/main/java/com/epam/finaltask/dto/VoucherDTO.java
@@ -2,10 +2,7 @@ package com.epam.finaltask.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -16,38 +13,42 @@ public class VoucherDTO {
 
     private String id;
 
-    @NotBlank
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "Description cannot be empty")
+    @Size(min = 10, max = 1000, message = "Description must be between 10 and 1000 characters")
     private String description;
 
-    @Positive
-    @NotNull
+    @Positive(message = "Price must be positive")
+    @NotNull(message = "Price cannot be empty")
+    @DecimalMin(value = "0.01", message = "Minimum price is 0.01")
+    @DecimalMax(value = "1000000.00", message = "Maximum price is 1,000,000")
     private Double price;
 
-    @NotBlank
+    @NotBlank(message = "Tour type cannot be empty")
     private String tourType;
 
-    @NotBlank
+    @NotBlank(message = "Transfer type cannot be empty")
     private String transferType;
 
-    @NotBlank
+    @NotBlank(message = "Hotel type cannot be empty")
     private String hotelType;
 
-    @NotBlank
+    @NotBlank(message = "Status cannot be empty")
     private String status;
 
-    @Future
+    @FutureOrPresent(message = "Arrival date cannot be in the past")
     private LocalDate arrivalDate;
 
-    @Future
+    @Future(message = "Eviction date must be in the future")
     private LocalDate evictionDate;
 
     @JsonIgnore
     private UUID userId;
 
-    @NotNull
+    @NotNull(message = "Hot field cannot be empty")
     @JsonProperty("hot")
     private Boolean hot;
 }
