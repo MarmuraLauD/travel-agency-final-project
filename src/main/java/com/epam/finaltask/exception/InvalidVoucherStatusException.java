dc4f71d6c47e217975cff3d0f1264acc21d2769a
LauD <marmuravlad10@gmail.com>
2026-04-15 15:05:37 +0300

    Added error classes.

---
+++ src/main/java/com/epam/finaltask/exception/InvalidVoucherStatusException.java
@@ -0,0 +1,8 @@
+package com.epam.finaltask.exception;
+
+public class InvalidVoucherStatusException extends RuntimeException {
+
+    public InvalidVoucherStatusException(String message) {
+        super(message);
+    }
+}
