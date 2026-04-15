1eb920029923fd7048d94fdb41cb7c3362623a36
LauD <marmuravlad10@gmail.com>
2026-04-15 12:30:07 +0300

    Added new methods for searching and filtering.

--- src/main/java/com/epam/finaltask/repository/VoucherRepository.java
+++ src/main/java/com/epam/finaltask/repository/VoucherRepository.java
@@ -7,8 +7,9 @@ import com.epam.finaltask.model.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface VoucherRepository extends JpaRepository<Voucher, UUID> {
+public interface VoucherRepository extends JpaRepository<Voucher, UUID>, JpaSpecificationExecutor<Voucher> {
     List<Voucher> findAllByUserId(UUID userId);
     Page<Voucher> findAllByTourType(TourType tourType, Pageable pageable);
     Page<Voucher> findAllByTransferType(TransferType transferType, Pageable pageable);
@@ -16,4 +17,6 @@ public interface VoucherRepository extends JpaRepository<Voucher, UUID> {
     Page<Voucher> findAllByHotelType(HotelType hotelType, Pageable pageable);
     Page<Voucher> findAllByStatus(VoucherStatus voucherStatus, Pageable pageable);
     Page<Voucher> findAllByHotTrue(Pageable pageable);
+    Page<Voucher> findAllByStatusAndHot(VoucherStatus status, boolean hot, Pageable pageable);
+    Page<Voucher> findAllByStatusAndTitleContainingIgnoreCase(VoucherStatus status, String title, Pageable pageable);
 }
